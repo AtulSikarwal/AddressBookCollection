14aa9dcc5de22f0a2573ffa09a7df3baa4fd481b
AtulSikarwal <atulsikarwal5.com>
2022-07-04 09:32:27 +0530

    The code for Create Contact

---
+++ CreateContact/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBookSystemDay23
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            AddressBook address = new AddressBook();
+            address.CreateContact();
+            address.Display();
+        }
+    }
+}
