9ad31eb386de99ca77b8a3f2aaa9e99378e7a30a
AtulSikarwal <atulsikarwal5.com>
2022-07-08 13:01:58 +0530

    The code for No Duplicate Data Entry

---
+++ NoDuplicateDataEntry/AddressBookSystem.cs
@@ -0,0 +1,16 @@
+﻿namespace AddressBookSystemDay23
+{
+    internal class AdressBookSystem
+    {
+        public Dictionary<string, AddressBook> adressBooks = new Dictionary<string, AddressBook>();
+        public void AddAddressBook()
+        {
+            AddressBook adressBook = new AddressBook();
+            adressBook.AddMultiple();
+            Console.WriteLine("enter the addressbook name");
+            string addressName = Convert.ToString(Console.ReadLine());
+            adressBooks.Add(addressName.ToLower(), adressBook);
+
+        }
+    }
+}
